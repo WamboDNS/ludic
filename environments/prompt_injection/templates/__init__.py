ce0009af41a912b627d7a110b6d9d71d9d89e5ad
Denis <deniswambold@gmail.com>
2026-01-23 10:24:03 +0100

    Remove prompt_injection code (moved to PIGAN repo)

--- environments/prompt_injection/templates/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Injection templates for curriculum bootstrap phase."""
-
-from .injections import INJECTION_TEMPLATES, get_random_template
-
-__all__ = ["INJECTION_TEMPLATES", "get_random_template"]
